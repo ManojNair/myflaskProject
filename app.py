1baa6331623caa1800abe5743a5adbf32a1a286f
Manoj Ravikumar Nair ([PS C:\]) <manonair@microsoft.com>
2021-09-15 15:10:59 +1000

    Reading from environment variable

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return os.environ['Cloud']
 
 
 if __name__ == '__main__':
