e032262a587c5239967f2c95f197f12093364338
Manoj Ravikumar Nair ([PS C:\]) <manonair@microsoft.com>
2021-09-15 15:23:16 +1000

    Updated the string to reflect awesomeness of Azure

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return os.environ['Cloud']
+    return f"{os.environ['Cloud']} is awesome!!!!"
 
 
 if __name__ == '__main__':
